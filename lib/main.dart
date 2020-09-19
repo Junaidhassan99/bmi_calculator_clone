f1e69b9e0c98661a0f1896a0dfa0b78ab7a32f65
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2020-09-19 13:21:39 +0500

    Dark theme/mode added.

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:bmi_calculator/screens/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainScreen());
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        /* light theme settings */
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        /* dark theme settings */
+      ),
+      themeMode: ThemeMode.dark, 
+      /* ThemeMode.system to follow system theme, 
+         ThemeMode.light for light theme, 
+         ThemeMode.dark for dark theme
+      */
+      home: HomeScreen(),
+    );
+  }
+}
