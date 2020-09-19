a4be3fcb2613981701586cd34cf08564b3cd3e50
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2020-09-19 17:54:34 +0500

    Updates.

--- lib/screens/results_screen.dart
+++ lib/screens/results_screen.dart
@@ -4,84 +4,78 @@ import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
   static const routeName = '/results_screen';
+  static const _defaultMargin = EdgeInsets.all(5);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        margin: EdgeInsets.all(5),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 1,
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              height: 70,
+              margin: _defaultMargin,
+              width: double.infinity,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Your Result',
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Card(
+              margin: _defaultMargin,
               child: Container(
-                height: 70,
-                width: double.infinity,
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Your Result',
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'Normal',
+                          style: TextStyle(color: Colors.green, fontSize: 20),
+                        ),
+                        Text(
+                          '22.1',
+                          style: TextStyle(fontSize: 60),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Normal BMI range:',
+                          style: TextStyle(fontSize: 22),
+                        ),
+                        Text(
+                          '18.5-25 kg/m2',
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'You have a normal body weight. Good job!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 22),
+                    ),
+                  ],
                 ),
               ),
             ),
-            Expanded(
-              flex: 4,
-              child: Card(
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            'Normal',
-                            style: TextStyle(color: Colors.green, fontSize: 20),
-                          ),
-                          Text(
-                            '22.1',
-                            style: TextStyle(fontSize: 60),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            'Normal BMI range:',
-                            style: TextStyle(fontSize: 22),
-                          ),
-                          Text(
-                            '18.5-25 kg/m2',
-                            style: TextStyle(fontSize: 22),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        'You have a normal body weight. Good job!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 22),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+          ),
+          Expanded(
+            child: GestureDetector(
+              child: BmiButton('RE-CALCULATE BMI'),
+              onTap: () => Navigator.of(context)
+                  .pushReplacementNamed(HomeScreen.routeName),
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                height: 2,
-              ),
-            ),
-            Expanded(
-              child: GestureDetector(
-                child: BmiButton('Re-CALCULATE BMI'),
-                onTap: () =>
-                    Navigator.of(context).pushReplacementNamed(HomeScreen.routeName),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
